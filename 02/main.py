51fdfeefb589dec893cb3be9022c49b070aea44a
jkurdziel <jakub.kurdziel@talkie.ai>
2021-10-30 13:18:02 +0200

    [02.16] Evaluate the following expressions.

---
+++ 02/main.py
@@ -0,0 +1,19 @@
+# After class
+
+print(f'a. The product of 15 and 38: {15 * 38} \n')
+
+print(
+    f'b. The product of the sum of the pairs of numbers 3 and 4, and 5 and 9: {(3 + 4) * (5 + 9)} \n')
+
+print(f'c. Integer part of dividing the numbers 7 and 2: {7 // 2} \n')
+
+print(f'd. The remainder of the division of 48 and 5: {48 % 5} \n')
+
+print(
+    f'e. Arithmetic mean of the numbers 8, 7, 4, 2: {(8 + 7 + 4 + 2) / 4} \n')
+
+print(f'f. 2^10: {2 ** 10} \n')
+
+print(f'g. Square root of 49 (do not use a function) {49 ** (1/2)} \n')
+
+print(f'h. 25 % of 80: {80 * 0.25} \n')
